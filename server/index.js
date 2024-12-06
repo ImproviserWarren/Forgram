a1be859348e8c6a77c5e691e19ec4e7cec621ba8
Snkenim <snekim288@gmail.com>
2024-12-06 19:51:40 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const connectDatabase = require('./utils/connectToDb.js')
-const postRoute = require('./routes/postRoute.js')
-const userRoute = require('./routes/userRoute.js')
-const commentRoute = require("./routes/commentRoute.js")
-const likePost = require("./routes/likeRoute.js")
- 
-const app = express()
-const port = process.env.port || 8080
-app.use(express.json())
-app.use(cors())
- 
-app.use(userRoute);
-app.use(postRoute)
-app.use(commentRoute)
-app.use(likePost)
+const express = require('express');
+const cors = require('cors');
+const connectDatabase = require('./utils/connectToDb.js');
+const postRoute = require('./routes/postRoute.js');
+const userRoute = require('./routes/userRoute.js');
+const commentRoute = require("./routes/commentRoute.js");
+const likePost = require("./routes/likeRoute.js");
 
-connectDatabase()
- 
+const app = express();
+const port = process.env.PORT || 8080; // Use PORT from environment or fallback to 8080
+
+app.use(express.json());
+app.use(cors());
+
+app.use(userRoute);
+app.use(postRoute);
+app.use(commentRoute);
+app.use(likePost);
+
+connectDatabase();
+
 app.listen(port, () => {
-    console.log(`your backend server is running on ${port}`)
-})
- 
+    console.log(`Your backend server is running on port ${port}`);
+});
